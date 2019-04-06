f0a46b1f8381d47c64aa16a0d324449c71fc5086
Usheninte <jrandangana@gmail.com>
2019-04-06 16:00:41 +0100

    built html, css & added image

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+const path = require('path')
+
 const express = require('express')
 
 const app = express()
 
 const port = 9000
 
+// Static files Middleware
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 app.use('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
+  res.send('<h1><em>Hello</em> World</h1>')
 })
 
 app.listen(port, () => console.log(`Server listening on ${port}`))
